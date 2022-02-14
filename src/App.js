882d3824637e7d33bfa3ff3ff2c88eebe8f7b57f
radibaraq <radibaraq@cloud.upwork.com>
2022-02-14 10:21:46 +0200

    setup ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: 'Car Or Vehicle', amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: 'New Desk Wooden', amount: 294.67, date: new Date(2021, 2, 28) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
     </div>
   );
 }
