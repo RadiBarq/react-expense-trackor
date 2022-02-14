eb53ba6a68e908e907dbfc90c81765e86bfae54e
radibaraq <radibaraq@cloud.upwork.com>
2022-02-14 11:04:16 +0200

    spliting components into multiple components

--- src/App.js
+++ src/App.js
@@ -14,22 +14,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
