dd6422c0dc417ad5f9f883797af84758d19248fa
radibaraq <radibaraq@cloud.upwork.com>
2022-03-23 18:28:35 +0200

    Using stateful lists

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: 'Car Or Vehicle', amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: 'New Desk Wooden', amount: 294.67, date: new Date(2021, 2, 28) },
+];
 
 const App = () => {
-  const expenses = [
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Car Or Vehicle', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'New Desk Wooden', amount: 294.67, date: new Date(2021, 2, 28) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {
-    console.log(expense)
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+  };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
