00856ba8f01c514f762267146c74c89314f844f7
radibaraq <radibaraq@cloud.upwork.com>
2022-03-23 14:48:34 +0200

    Randering Lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,44 +1,34 @@
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter';
-import { useState } from 'react';
+  import ExpenseItem from './ExpenseItem';
+  import Card from '../UI/Card';
+  import './Expenses.css';
+  import ExpensesFilter from './ExpensesFilter';
+  import { useState } from 'react';
 
-const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState('2022');
+  const Expenses = (props) => {
+    const [selectedYear, setSelectedYear] = useState('2022');
 
-  const filterChangeHandler = (selectedYear) => {
-    setSelectedYear();
-    console.log(selectedYear);
+    const filterChangeHandler = (selectedYear) => {
+      setSelectedYear();
+      console.log(selectedYear);
+    };
+
+    return (
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter
+            selectedYear={selectedYear}
+            onChangeFilter={filterChangeHandler}
+          />
+          {props.items.map((expense) => (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        </Card>
+      </div>
+    );
   };
 
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter selectedYear={selectedYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-      </Card>
-    </div>
-  );
-};
-
-export default Expenses;
+  export default Expenses;
