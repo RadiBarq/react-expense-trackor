b2ad70ee801b04af9d910bca78e6479877b04f5f
radibaraq <radibaraq@cloud.upwork.com>
2022-04-14 14:57:52 +0300

    Add conditional return statments

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import { useState } from 'react';
 
 const Expenses = (props) => {
@@ -13,20 +13,7 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(
     (expense) => expense.date.getFullYear().toString() === selectedYear
-  );
-
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  ); 
 
   return (
     <div>
@@ -35,7 +22,7 @@ const Expenses = (props) => {
           selectedYear={selectedYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList expenses = {filteredExpenses}/>
       </Card>
     </div>
   );
