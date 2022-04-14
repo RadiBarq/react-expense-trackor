a83ab361c88e5fc598175970dc740f27612ebb79
radibaraq <radibaraq@cloud.upwork.com>
2022-04-14 17:42:04 +0300

    Add expenses chart for each month

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ChartBar from './ChartBar';
-import 'ChartBar.css';
+import './Chart.css';
 
 const Chart = (props) => {
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointsValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.id}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import { useState } from 'react';
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
           selectedYear={selectedYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList expenses = {filteredExpenses}/>
       </Card>
     </div>
